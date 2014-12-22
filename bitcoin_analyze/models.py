86c9fc7a09064c463e327c1b61e646f9246d2700
cemkiy <se.cemkiy@gmail.com>
2014-12-22 21:37:29 +0200

    KS-02 btc analyze models  -added models

--- bitcoin_analyze/models.py
+++ bitcoin_analyze/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+__author__ = 'cemkiy'
 # Create your models here.
+
+class rank(models.Model):
+    twitter = models.FloatField(default=0.0)
+    expected_value = models.FloatField(default=0.0)
+    volume = models.FloatField(default=0.0)
+    change = models.FloatField(default=0.0)
+    change_rate = models.FloatField(default=0.0)
+
+
+class twitter(models.Model):
+    processed_tweets_number = models.SmallIntegerField(default=100)
+    positive_tweet_number = models.SmallIntegerField()
+    negative_tweet_number = models.SmallIntegerField()
+    result = models.BooleanField(default=True)  # main result
+
+
+class expected_value(models.Model):
+    high = models.FloatField(default=0.0)
+    low = models.FloatField(default=0.0)
+    avg = models.FloatField(default=0.0)
+    result = models.BooleanField(default=True)  # main result
+
+
+class volume(models.Model):
+    volume_value = models.FloatField(default=0.0)
+    result = models.BooleanField(default=True)  # main result
+
+
+class change(models.Model):
+    change_value = models.FloatField(default=0.0)  # 3.15
+    result = models.BooleanField(default=True)  # main result
+
+
+class change_rate(models.Model):
+    change_rate_value = models.FloatField(default=0.0)  # % 0.41
+    result = models.BooleanField(default=True)  # main result
