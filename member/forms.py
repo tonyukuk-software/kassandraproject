440abe1316ce9edd7990547c00e2f4e7d0677e3e
barisariburnu <barisariburnu@gmail.com>
2014-12-28 12:38:27 +0200

    KS-17 Upload Profile Photo  - created edit_profile_photo.html  - added edit_profile_photo function  - added edit_profile_photo_form

--- member/forms.py
+++ member/forms.py
@@ -10,3 +10,6 @@ class new_member_form(forms.Form):
     password = forms.CharField(max_length=15, widget=forms.PasswordInput())
     first_name = forms.CharField(max_length=30, widget=forms.TextInput)
     last_name = forms.CharField(max_length=30, widget=forms.TextInput)
+
+class edit_profile_photo_form(forms.Form):
+    profile_photo = forms.ImageField()
--- member/views.py
+++ member/views.py
@@ -38,6 +38,29 @@ def member_profile(request):
         print e
         return HttpResponseRedirect('/sorry')
 
+@login_required
+def edit_profile_photo(request):
+    try:
+        member = User.objects.filter(username=request.user.username)[0]
+    except Exception as e:
+        print e
+        return HttpResponseRedirect('/sorry')
+
+    form = edit_profile_photo_form()
+
+    if request.method == 'POST':
+        form = edit_profile_photo_form(request.POST, request.FILES)
+        if form.is_valid():
+            try:
+                member.profile_photo = request.FILES["profile_photo"]
+                member.save()
+                return HttpResponseRedirect('/member/member_profile')
+            except Exception as e:
+                print e
+                return HttpResponseRedirect('/sorry')
+
+    return render_to_response('edit_profile_photo.html', locals(), context_instance=RequestContext(request))
+
 def success_url(request):
     pass
 
